0e532d1755a01aa90bf1fa981792a971915714aa
Devendra Singh <devendra.tarento@uidai.com>
2019-06-14 17:53:32 +0530

    Implemented redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,32 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async dispatch => {
+    
     const response = await jsonPlaceholder.get('/posts');
+    dispatch({type: 'FETCH_POSTS', payload: response})
 
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    }; 
-};
+}
+
+
+
+
+// function createThunkMiddleware(extraArgument) {
+//     return ({ dispatch, getState }) => {
+//         return  (next) => {
+//             return (action) => {
+//                 if (typeof action === 'function') {
+//                     return action(dispatch, getState, extraArgument);
+//                 }
+            
+//                 return next(action);
+//             };
+
+//         }
+//     }
+
+//   }
+  
+//   const thunk = createThunkMiddleware();
+//   thunk.withExtraArgument = createThunkMiddleware;
+  
+//   export default thunk;
